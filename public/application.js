5b386c4adb7fbd4d4b3a1707c015d47f328b1f64
culling <culling66@gmail.com>
2016-11-24 18:56:28 +0800

    remove commented code

--- public/application.js
+++ public/application.js
@@ -2,6 +2,6 @@ var mainApplicationModuleName   = "projectApp";
 var MainApplicationModule       = angular.module(mainApplicationModuleName, ['ngResource',  'nvd3', 'projects']);
 
 angular.element(document).ready(function (){
-    //angular.bootstrap(document, [mainApplicationModuleName]);
+
 });
 
